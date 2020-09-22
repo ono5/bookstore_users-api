cc7a2d86e7938cd7cc5c6f3972d281e26ada4628
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-09-23 07:28:13 +0900

    fix: Added unique key for date_created

--- db/sql/001-create-tables.sql
+++ db/sql/001-create-tables.sql
@@ -4,8 +4,7 @@ create table IF not exists `users`
  `id`               BIGINT(20) AUTO_INCREMENT NOT NULL,
  `first_name`       VARCHAR(45),
  `last_name`        VARCHAR(45),
- `email`            VARCHAR(45) NOT NULL,
+ `email`            VARCHAR(45) UNIQUE NOT NULL,
  `date_created`     VARCHAR(45),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (`date_created`)
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
